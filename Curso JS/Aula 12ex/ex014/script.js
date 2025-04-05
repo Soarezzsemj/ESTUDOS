1e0fcc2abae30490178cfd2bdc8c7325790efe27
Soarezzsemj <popcornor6topster@outlook.com>
2025-04-04 21:42:21 -0300

    first commit

---
+++ Curso JS/Aula 12ex/ex014/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    
+   msg.innerHTML = `agora sao ${hora} horas`
+
+   if(hora >=0 && hora < 12){
+    img.src = 'manha.png'
+    document.body.style.background = '#df836e'
+   } else if(hora >= 12 && hora < 18) {
+    img.src = 'tarde.png'
+    document.body.style.background = '#845f35'
+   }
+   else {
+    img.src = 'Noite.png'
+    document.body.style.background = '#06427e'
+   }
+}
