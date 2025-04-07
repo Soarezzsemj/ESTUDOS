4cefe0ea0c45ce4fc3e0a050d1b8f9642cb8616e
Soarezzsemj <popcornor6topster@outlook.com>
2025-04-06 23:57:21 -0300

    2

--- Aula 11/ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 120
-console.log(`a velocidade do seu carro é ${vel} Km/h`)
-if(vel > 60 ){ // condição simples
-    console.log(`MULTADO`)
-}
-console.log(`dirija sempre com cinto de segurança!`)
--- Aula 12/ex011.js
+++
@@ -1,9 +0,0 @@
-var idade = 17
-console.log(`voce tem ${idade} anos.`)
-if (idade < 16){
-    console.log(`Nao vota`)
-} else if (idade < 18 || idade > 65){
-    console.log(`voto opcional`)
-} else {
-    console.log(`voto obrigatorio`)
-}
